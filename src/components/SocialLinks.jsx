fb5d8a256b8c8e3f222893bc5df8809f8a2ba665
Vaishali Jain <94951827+jainvaishali0408@users.noreply.github.com>
2023-07-15 23:11:19 +0530

    Update SocialLinks.jsx
    third commit

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "https://drive.google.com/file/d/18HsCu5fI04KX_N0GPis18z6UrZhslklf/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1xdqeeSsmwOmxOXLEssT4BxH04muChnGj/view?usp=sharing",
       style: "rounded-br-md",
     },
   ];
